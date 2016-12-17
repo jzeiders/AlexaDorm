ff3a01d15a69847a189c7198f6fcb20c4b046a53
Jack Zeiders <zeiders@usc.edu>
2016-12-17 14:41:24 -0600

    Edited variables

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,72 +2,65 @@
 
 var pg = require('pg');
 
-var pgConfig = {
-	user: 'alexadorm', //env var: PGUSER
-	database: 'data', //env var: PGDATABASE
-	password: process.env.AWSPASS, //env var: PGPASSWORD
-	host: 'addb.cfj01akmcn5a.us-west-2.rds.amazonaws.com', // Server hosting the postgres database
-	port: 5432, //env var: PGPORT
-	max: 10, // max number of clients in the pool
-	idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-};
-var pool = new pg.Pool(pgConfig);
+process.env.PGUSER = 'alexadorm';
+process.env.PGDATABASE = 'data';
+process.env.PGPASSWORD = process.env.AWSPASS;
+process.env.PGHOST = 'addb.cfj01akmcn5a.us-west-2.rds.amazonaws.com'
+process.env.port = 5432;
 
-
-var query = function(query){
-  return new Promise(function(res,rej){
-    client.query(query, function(err, data){
-      if(err) {
-        console.log(err);
-        rej(err)};
-      res(data);
-    });
-  });
-};
-
-var client;
-pool.connect(function(err,_client,done){
-  if(err){
-    console.log(err);
-  }
-  client = _client
+var client = new pg.Client();
+client.connect(function(err) {
+	if (err) console.log(err);
 });
-exports.createTables = function(){
-  var string = "CREATE TABLE groups (name text NOT NULL, members text[])";
-  return query(string);
-};
-exports.dropTables = function(){
-  var string = "DROP TABLE groups";
-  return query(string);
+
+var query = function(query) {
+	return new Promise(function(res, rej) {
+		client.query(query, function(err, data) {
+			if (err) {
+				console.log(err);
+				rej(err);
+			};
+			res(data);
+		});
+	});
 };
 
-exports.getGroups = function(){
-  var string = "SELECT name FROM groups";
-  return query(string).then(function(data){
-    return data.rows.map((v) => v.name);
-  });
+exports.createTables = function() {
+	var string = "CREATE TABLE groups (name text NOT NULL, members text[])";
+	return query(string);
+};
+exports.dropTables = function() {
+	var string = "DROP TABLE groups";
+	return query(string);
 };
 
-exports.getGroup = function(name){
-  var string = "SELECT * FROM groups WHERE name='"+name+"';";
-  return query(string).then(function(data){
-    return data.rows;
-  });
+exports.getGroups = function() {
+	var string = "SELECT name FROM groups";
+	return query(string).then(function(data) {
+		return data.rows.map((v) => v.name);
+	});
 };
-exports.addMember = function(number,group){
-  var string = "UPDATE groups SET members = members || ARRAY['" + number + "'] WHERE name='"+ group+"'";
-  return query(string);
+
+exports.getGroup = function(name) {
+	var string = "SELECT * FROM groups WHERE name='" + name + "';";
+	return query(string).then(function(data) {
+		return data.rows;
+	});
 };
-exports.addGroup = function(name){
-  var string = "INSERT INTO groups (name,members) VALUES ('" + name + "', ARRAY[]::integer[]);";
-  return query(string);
+exports.addMember = function(number, group) {
+	var string = "UPDATE groups SET members = members || ARRAY['" + number + "'] WHERE name='" + group + "'";
+	return query(string);
 };
-exports.deleteGroup = function(name){
-  var string = "DELETE FROM groups WHERE name='"+name+"';";
-  return query(string);
+exports.addGroup = function(name) {
+	var string = "INSERT INTO groups (name,members) VALUES ('" + name + "', ARRAY[]::integer[]);";
+	return query(string);
+};
+exports.deleteGroup = function(name) {
+	var string = "DELETE FROM groups WHERE name='" + name + "';";
+	return query(string);
 }
-exports.deleteMember = function(number,group){
-  var string = "UPDATE groups SET members = array_remove(members,'" + number+"') WHERE name='"+group+"'";
-  console.log(string);
-  return query(string);
+exports.deleteMember = function(number, group) {
+	var string = "UPDATE groups SET members = array_remove(members,'" + number + "') WHERE name='" + group + "'";
+	console.log(string);
+	return query(string);
 };
